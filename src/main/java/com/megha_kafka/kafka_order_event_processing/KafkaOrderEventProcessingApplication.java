56204147abbf6c5145543c6a09aa726614fba90c
Meghana-Madala <meghanamadala17@gmail.com>
2026-06-18 22:39:58 -0500

    Initial Kafka order processing project setup

---
+++ src/main/java/com/megha_kafka/kafka_order_event_processing/KafkaOrderEventProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.megha_kafka.kafka_order_event_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderEventProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOrderEventProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megha_kafka/kafka_order_event_processing/KafkaOrderEventProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megha_kafka.kafka_order_event_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderEventProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
